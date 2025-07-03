84d97ded3fd280bc1402ac5936f7cf64ea2875e9
stlnc <curceac@fzp.czu.cz>
2025-07-03 12:16:47 +0200

    Create main.R

---
+++ source/main.R
@@ -0,0 +1,11 @@
+## --- Packages ---
+suppressPackageStartupMessages({
+  library(data.table)
+  library(lubridate)
+  library(ggplot2)
+  library(raster)
+  library(ncdf4)
+  library(sp)
+  library(sf)
+  library(stars)
+})
