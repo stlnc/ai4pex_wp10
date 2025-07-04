6fa360d62140050c679d9cb5aedbe3e7196b6d2c
stlnc <curceac@fzp.czu.cz>
2025-07-04 16:11:44 +0200

    update main.R

--- source/main.R
+++ source/main.R
@@ -9,3 +9,10 @@ suppressPackageStartupMessages({
   library(sf)
   library(stars)
 })
+
+## --- Base Paths ---
+PATH_ROOT <- normalizePath("~/storage/brno2/home/curceac", mustWork = FALSE)
+PATH_DATA <- file.path(PATH_ROOT, "data")
+PATH_SAVE <- file.path(PATH_ROOT, "data_project")
+
+PATH_PREC_SIM <- file.path(PATH_DATA, "sim/precip/raw")
